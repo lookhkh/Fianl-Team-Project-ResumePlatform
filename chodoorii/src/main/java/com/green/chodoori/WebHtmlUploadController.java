c951e0ee40147afffd016f4feb454d000f3982a0
lookhkh <62491156+lookhkh@users.noreply.github.com>
2021-07-13 12:07:18 +0900

    Add files via upload
    Thymeleaf Dependency added

--- chodoorii/src/main/java/com/green/chodoori/WebHtmlUploadController.java
+++ chodoorii/src/main/java/com/green/chodoori/WebHtmlUploadController.java
@@ -3,6 +3,7 @@ package com.green.chodoori;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,36 +11,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @CrossOrigin("*")
-@RequestMapping("/upload")
+@RequestMapping("/")
 public class WebHtmlUploadController {
 
-	@Autowired
-	Repo repo;
 	
-	@PostMapping
-	public String test(@RequestBody byte[] html) {
-System.out.println("");
-		System.out.println(html);
-		System.out.println(html.toString().length());
-		repo.repo.put("1", html);
-		System.out.println(html.length);
-		System.out.println(html);
-		
-
-
-		
-		
-		return "ok";
-		
+	@GetMapping("/hi")
+	public String test() {
+		System.out.println("호출");
+		return "test";
 	}
-	
-	@GetMapping
-	public byte[] getTest(HttpServletResponse res) {
-		res.setHeader("Content-type", "application/octet-stream");
-		System.out.println("getmapping ȣ��");
-		return repo.repo.get("1");
-	}
-	
+
 }
