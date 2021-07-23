d98e5e1dbfcefd86566597707be0204b16c2f242
DESKTOP-ASEHIMO\jomin <lookhkh37@gmail.com>
2021-07-23 16:02:21 +0900

    비밀번호,id찾기 기능 추가 완료료

--- chodoorii/src/main/java/com/green/chodoori/main/controller/EmailAuthController.java
+++ chodoorii/src/main/java/com/green/chodoori/main/controller/EmailAuthController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.green.chodoori.util.mail.MailAuthInfoDto;
@@ -51,6 +52,40 @@ public class EmailAuthController {
 		return storedAuthNum.equals(authNum)?new ResponseEntity<String>(HttpStatus.OK):new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 	}
 	
+	@GetMapping("/lookupid/{email}")
+	public ResponseEntity<String> sendIdtoGivenEmail(@PathVariable String email){
+		//email을 통하여 유저 정보 가져오는 로직 작성
+		System.out.println("호출됨");
+		String tempId = "lookhkh";
+		
+		try {
+		service.sendMailForIdLookUp(email, tempId);
+		return ResponseEntity.ok("요청이 완료되었습니다");
+		
+			}
+		catch(Exception e) {
+			return ResponseEntity.internalServerError().body("문제가 발생하였습니다");
+		}
+		
+	}
+	
+	@GetMapping("/lookuppw")
+	public ResponseEntity<String> sendPwtoGivenEmail(@RequestParam("email")String email, @RequestParam("id") String id){
+		
+		//아이디, 이메일로 검증로직 작성
+		System.out.println("비번찾기 요청됨");
+		String tempPw = "1234";
+		
+		
+		
+		try {
+			service.sendMailForPasswordLookUp(email,id ,tempPw);
+			return ResponseEntity.ok("요청이 성공적으로 접수되었습니다");
+		}catch(Exception e) {
+			return ResponseEntity.internalServerError().body("요청에 실패하였습니다");
+
+		}
+	}
 	
 
 
