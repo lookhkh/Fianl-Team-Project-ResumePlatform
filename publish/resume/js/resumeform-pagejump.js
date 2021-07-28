a4a2a9284683ddc04f90816d16fa959eec7786f5
DESKTOP-ASEHIMO\jomin <lookhkh37@gmail.com>
2021-07-28 14:25:41 +0900

    이력서 공통 폼 제작 중

---
+++ publish/resume/js/resumeform-pagejump.js
@@ -0,0 +1,55 @@
+window.addEventListener('load',()=>{
+    ////////////////////////////////////////버튼 이동///////
+    const current_page_lists = ["first","second","third"];
+    let current_page_num = 0;
+    let allSection = document.querySelectorAll('section');
+
+
+    const formTag = document.querySelector('form');
+
+        formTag.addEventListener('click',(e)=>{
+            const target = e.target;
+            let current_page;
+            if(target.nodeName!=="BUTTON") return;
+            
+            e.preventDefault();
+            
+
+            if(target.classList.contains("next-btn")){
+            
+                current_page_num = (current_page_num+1)%3;
+
+                current_page ="."+current_page_lists[current_page_num]+"-form";
+
+
+            }else if(target.classList.contains("previous-btn")){
+                                
+            
+                current_page_num = (current_page_num-1)%3;
+
+                current_page ="."+current_page_lists[current_page_num]+"-form";
+            }
+
+            if(target.classList.contains("submit-btn")){
+                if(confirm("이력서를 최종 제출하시겠습니까?")) formTag.submit();
+            }
+
+            if(target.classList.contains("preview-btn")){
+                alert("미리보기 기능 구현해야 함")
+            }
+
+            let current_section = document.querySelector(current_page);
+                
+
+                current_section.classList.remove('hidden');
+                allSection.forEach(a=>{
+                    if(a!==current_section&&!a.classList.contains('hidden')){
+                        a.classList.add("hidden")
+                    }
+                })
+        })
+//////////////////////////////버튼 이동//////////////////////////
+
+
+
+})
