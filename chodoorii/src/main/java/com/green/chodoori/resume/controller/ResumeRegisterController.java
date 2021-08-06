5e4084501181fc2420dc856d5897e349dec31534
jiniii227 <hyojiniii227@gmail.com>
2021-08-06 15:37:48 +0900

    임시

--- chodoorii/src/main/java/com/green/chodoori/resume/controller/ResumeRegisterController.java
+++ chodoorii/src/main/java/com/green/chodoori/resume/controller/ResumeRegisterController.java
@@ -1,21 +1,42 @@
 package com.green.chodoori.resume.controller;
 
-import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
+import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.green.chodoori.main.domain.UserInfoDto;
+import com.green.chodoori.main.domain.UserInfoRepo;
+import com.green.chodoori.main.web.domain.SessionUserInfo;
+import com.green.chodoori.resume.domain.IntroductionDto;
+import com.green.chodoori.resume.domain.ResumeDto;
+import com.green.chodoori.resume.domain.ResumeDtoRepo;
+import com.green.chodoori.resume.domain.SkillSetDto;
 import com.green.chodoori.resume.domain.SnsAddressDto;
+import com.green.chodoori.util.fileUpload.ImgUploadAndGenerateSignUpDto;
 
 @Controller
 @RequestMapping("/resume")
 public class ResumeRegisterController {
+	
+	@Autowired
+	ImgUploadAndGenerateSignUpDto imgService;
+	
+	@Autowired
+	ResumeDtoRepo resumeRepo;
+	
+	@Autowired
+	UserInfoRepo userRepo;
 	
 	@GetMapping("/template")
 	public String templateChoicePage() {
@@ -28,55 +49,143 @@ public class ResumeRegisterController {
 		return "/resume/resumeForm";
 	}
 	
+	@Transactional
 	@ResponseBody
 	@PostMapping("/form")
 	public String templageFormInputCheck(HttpSession session,
-										@RequestParam String template_kind, 
-										@RequestParam String introduction_header,
-										@RequestParam String introduction_main,
-										@RequestParam MultipartFile introduction_img_path,
-										@RequestParam String[] skil_set,									
-										@ModelAttribute SnsAddressDto sns,
-										@RequestParam String disclosure,
-										@RequestParam MultipartFile portfolio_first_img,
-										@RequestParam String portfolio_first_name,
-										@RequestParam String portfolio_first_github,
-										@RequestParam String portfolio_first_desc,
-										@RequestParam MultipartFile portfolio_second_img,
-										@RequestParam String portfolio_second_name,
-										@RequestParam String portfolio_second_github,
-										@RequestParam String portfolio_second_desc,
-										@RequestParam MultipartFile portfolio_third_img,
-										@RequestParam String portfolio_third_name,
-										@RequestParam String portfolio_third_github,
-										@RequestParam String portfolio_third_desc) {
+										@RequestParam(required = false) String template_kind, 
+										@RequestParam(required = false) String introduction_header,
+										@RequestParam(required = false) String introduction_main,
+										@RequestParam(required = false) MultipartFile introduction_img_path,
+										@RequestParam(required = false) String[] skil_set,									
+										@RequestParam(required = false) String blog_address,
+										@RequestParam(required = false) String github_address,
+										@RequestParam(required = false) String instagram_address,
+										@RequestParam(required = false) String facebook_address,
+										@RequestParam(required = false, defaultValue = "off") String disclosure,
+										@RequestParam(required = false) MultipartFile portfolio_first_img,
+										@RequestParam(required = false) String portfolio_first_name,
+										@RequestParam(required = false) String portfolio_first_github,
+										@RequestParam(required = false) String portfolio_first_desc,
+										@RequestParam(required = false) MultipartFile portfolio_second_img,
+										@RequestParam(required = false) String portfolio_second_name,
+										@RequestParam(required = false) String portfolio_second_github,
+										@RequestParam(required = false) String portfolio_second_desc,
+										@RequestParam(required = false) MultipartFile portfolio_third_img,
+										@RequestParam(required = false) String portfolio_third_name,
+										@RequestParam(required = false) String portfolio_third_github,
+										@RequestParam(required = false) String portfolio_third_desc,
+										Model model) throws IllegalStateException, IOException {
 		
-		
-		System.out.println(template_kind+"  "+introduction_header+"  "+introduction_main);
-		System.out.println(introduction_img_path);
-		System.out.println(sns.toString());
-		System.out.println(disclosure);
 
-		for(int i=0; i<skil_set.length; i++) {
-			System.out.println(skil_set[i]);
+
+		
+		
+		ResumeDto resume = new ResumeDto();
+		
+		SnsAddressDto sns = new SnsAddressDto();
+		sns.setBlog_address(blog_address);
+		sns.setFacebook_address(facebook_address);
+		sns.setGithub_address(github_address);
+		sns.setInstagram_address(instagram_address);
+		
+		resume.setSns_dto(sns);
+		
+		IntroductionDto intro = new IntroductionDto();
+		intro.setIntroduction_header(introduction_header);
+		intro.setIntroduction_main(introduction_main);
+		
+		if(!introduction_img_path.isEmpty()) {
+		intro.setIntroduction_img_path(imgService.imgUploadAndGenerateSignUpDto(introduction_img_path));
 		}
 		
-		System.out.println(portfolio_first_img);
-		System.out.println(portfolio_first_name);
-		System.out.println(portfolio_first_github);
-		System.out.println(portfolio_first_desc);
-		System.out.println(portfolio_second_img);
-		System.out.println(portfolio_second_name);
-		System.out.println(portfolio_second_github);
-		System.out.println(portfolio_second_desc);
-		System.out.println(portfolio_third_img);
-		System.out.println(portfolio_third_name);
-		System.out.println(portfolio_third_github);
-		System.out.println(portfolio_third_desc);
+		resume.setIntro_dto(intro);
+		
 
 		
+		SkillSetDto skill = new SkillSetDto();
 		
-		return "hi";
+		if(skil_set.length>=1&&skil_set[0]!=null) {
+			skill.setSkill_set_first(skil_set[0]);
+		}
+		if(skil_set.length>=2&&skil_set[1]!=null) {
+			skill.setSkill_set_second(skil_set[1]);
+		}
+		if(skil_set.length>=3&&skil_set[2]!=null) {
+			skill.setSkill_set_third(skil_set[2]);
+		}
+		if(skil_set.length>=4&&skil_set[3]!=null) {
+			skill.setSkill_set_forth(skil_set[3]);
+		}
+		if(skil_set.length>=5&&skil_set[4]!=null) {
+			skill.setSkill_set_fifth(skil_set[4]);
+		}
+		if(skil_set.length>=6&&skil_set[5]!=null) {
+			skill.setSkill_set_sixth(skil_set[5]);
+		}
+		if(skil_set.length>=7&&skil_set[6]!=null) {
+			skill.setSkill_set_seventh(skil_set[6]);
+		}
+		if(skil_set.length>=8&&skil_set[7]!=null) {
+			skill.setSkill_set_eieth(skil_set[7]);
+		}
+		
+		
+		resume.setSkill_dto(skill);
+		
+		
+
+		resume.setTemplate_kind(template_kind);
+		
+		
+		
+		resume.setDisclosurestatus(disclosure.equals("on")?0:1);
+	
+		resume.setPortfolio_first_name(portfolio_first_name);
+		resume.setPortfolio_first_github(portfolio_first_github);
+		resume.setPortfolio_first_desc(portfolio_first_desc);
+		if(!portfolio_first_img.isEmpty()) {
+		String imgPathForFirst= imgService.imgUploadAndGenerateSignUpDto(portfolio_first_img);
+		resume.setPortfolio_first_img(imgPathForFirst);
+
+		}
+
+		
+		resume.setPortfolio_second_desc(portfolio_second_desc);
+		resume.setPortfolio_second_github(portfolio_second_github);
+		resume.setPortfolio_second_name(portfolio_second_name);
+		
+
+		if(!portfolio_second_img.isEmpty()) {
+		resume.setPortfolio_second_img(imgService.imgUploadAndGenerateSignUpDto(portfolio_second_img));
+		}
+		
+		resume.setPortfolio_third_desc(portfolio_third_desc);
+		resume.setPortfolio_third_github(portfolio_third_github);
+		resume.setPortfolio_third_name(portfolio_third_name);
+		
+
+		
+		if(!portfolio_third_img.isEmpty()) {
+		resume.setPortfolio_third_img(imgService.imgUploadAndGenerateSignUpDto(portfolio_third_img));
+		}
+		
+		SessionUserInfo sessionUser = (SessionUserInfo) session.getAttribute("userInfo");
+		UserInfoDto user = userRepo.getById(sessionUser.getId());
+		
+		resume.setUser(user);
+		
+		resumeRepo.save(resume);
+		
+		session.setAttribute("temp", resume);
+		
+		System.out.println(resumeRepo.findById(user.getId()).toString());
+		
+		model.addAttribute("temp",resume);
+		
+		String templateName = "/resume/template/templateSample"+template_kind;
+		
+		return templateName;
 	}
 	
 
